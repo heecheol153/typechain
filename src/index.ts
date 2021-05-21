b74589d3a94d6a74a21bf66cadcd64216693b50f
heecheol153 <heecheol153@gmail.com>
2021-05-21 10:42:52 +0900

    #12 Validating Block Structure part Two

--- src/index.ts
+++ src/index.ts
@@ -64,8 +64,17 @@ const createNewBlock = (data: string): Block => {
     data,
     newTimestamp
   );
+  addBlock(newBlock);
   return newBlock;
 };
+
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
 
 // candidateBlock, previousBlock를 비교한다.
 const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
@@ -75,6 +84,16 @@ const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
     return false;
   } else if (previousBlock.hash !== candidateBlock.previousHash) {
     return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
   }
 };
 
